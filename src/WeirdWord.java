19089a5f657424481adef0e38e270d1f113eaae4
DOGWANNA <9401ndk@naver.com>
2023-03-13 16:55:21 +0900

    feat: 알고리즘 문제 풀이 추가

--- src/WeirdWord.java
+++ src/WeirdWord.java
@@ -38,7 +38,7 @@ public class WeirdWord {
                     }
                 }else {
                     break;
-                }//sadfsd
+                }//sadfsdd
             }
             if(!split[i].equals("")) answer += " ";
         }
